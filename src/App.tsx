4795d9fb065a1ba864afc4eab4d1a5a16a14b810
louisaspicer <louisa.spicer@echo.co.uk>
2020-03-04 21:38:51 +0000

    Setup app with navigation dependencies

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+};
+
+export default App;
